4d715c5fbdcafff52181dc7d34de4b098a5d455e
Fernando Oliveira <fernandoaju78@gmail.com>
2024-01-28 13:40:23 -0300

    recovery settings saved after format data
    After format data, the settings were not being saved on devices with the "TW_INCLUDE_CRYPTO" flag, so I returned the line to the previous path and included a condition to execute the other line only when the flag is not set.
    
    Change-Id: I05461f9dfd5a21c7353e80dc464902603517ac3d
    Signed-off-by: Fernando Oliveira <fernandoaju78@gmail.com>
    Signed-off-by: rezaadi0105 <rezaadipangestu5@gmail.com>

--- partitionmanager.cpp
+++ partitionmanager.cpp
@@ -3191,6 +3191,7 @@ bool TWPartitionManager::Decrypt_Adopted() {
 		}
 	}
 
+	DataManager::SetValue("tw_settings_path", TW_STORAGE_PATH);
 	LOGINFO("Decrypt adopted storage starting\n");
 	char* xmlFile = PageManager::LoadFileToBuffer(path, NULL);
 	xml_document<> *doc = NULL;
